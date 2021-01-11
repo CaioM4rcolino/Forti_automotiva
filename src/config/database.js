85b5a391075a9f8ab5a17b20c06be2f76af1f0a9
CaioM4rcolino <67704957+CaioM4rcolino@users.noreply.github.com>
2021-01-11 17:39:13 -0300

    Add files via upload

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    host: "localhost",
+    username: "root",
+    password: "marcolinobd",
+    database:"senai_overflow",
+    dialect: "mysql",
+    define: {
+
+        timestamp: true,
+        underscored: true
+    }
+
+}
